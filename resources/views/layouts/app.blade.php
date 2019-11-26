b764864c128342404671a4c59b63de84a2140bc0
evaw16 <43206496+evaw16@users.noreply.github.com>
2019-11-26 20:48:57 +0700

    logo

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,10 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+              <img src="{{asset('img/logo.png')}}" class="img-circle" height="100" width="100">
+                <!-- <a class="navbar-brand" href="{{ url('/') }}">
                     Sambat
-                </a>
+                </a> -->
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
